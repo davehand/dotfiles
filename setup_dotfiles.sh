95736bd58ab71f2131bb4780923858dca6da9978
davehand <davehand13@gmail.com>
2015-05-11 15:17:46 -0400

    Adjusted to bash, using for loop

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
 #setup script for putting all dotfiles in home directory
 #Author: Dave Hand
 #Requires dotfiles repo from github
 
-#symlink all the files to the home directory
-#TODO simply have this iterate over all dotfiles
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.prompt_bash ~/.prompt_bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+#iterate over all dotfiles
+for f in .[^.]*; do
+  if [ -f $f ]; then #only care about files
+    HOME_FILE="$HOME/$f"
+    if [ -e $HOME_FILE ]; then #remove any existing file
+      rm $HOME_FILE #remove
+    fi
+    ln -s $HOME/dotfiles/$f $HOME_FILE #symlink
+  fi
+done
 
 #source the bash files
 source ~/.bash_profile
 
 #setup vim
-sh setup_vim.sh
+./setup_vim.sh
