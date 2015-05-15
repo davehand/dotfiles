e1d57fac65b4fbecda60ea8b276ae9f1f12fcbc7
Dave Hand <davehand@users.noreply.github.com>
2015-05-14 20:25:53 -0400

    Updates install script, README, added gitignore

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ System configuration files for Mac/Linux
 
 ##### Setup instructions:
     git clone https://github.com/davehand/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
     ./install.sh
--- install.sh
+++ install.sh
@@ -3,19 +3,39 @@
 #Author: Dave Hand
 #Requires dotfiles repo from github
 
+#make backup directory for storing already existent system files
+if [ ! -e $HOME/.dotfiles/backups ]; then
+  mkdir backups
+fi
+
+#Get homebrew installed
+
 #iterate over all dotfiles
 for f in .[^.]*; do
   if [ -f $f ]; then #only care about files
     HOME_FILE="$HOME/$f"
     if [ -e $HOME_FILE ]; then #remove any existing file
-      rm $HOME_FILE #remove
+      mv $HOME_FILE $HOME/.dotfiles/backups #move to backup directory
     fi
-    ln -s $HOME/dotfiles/$f $HOME_FILE #symlink
+    echo new link $HOME/.dotfiles/$f to $HOME_FILE
+    ln -s $HOME/.dotfiles/$f $HOME_FILE #symlink
   fi
 done
 
-#source the bash files
-source ~/.bash_profile
-
 #setup vim
-./setup_vim.sh
+#first, clone the vundle repository
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+#then, install files specified by vundle
+vim +PluginInstall +qall
+
+#then, link molokai to the colors directory
+if [ ! -e $HOME/.vim/colors ]; then
+  mkdir $HOME/.vim/colors
+fi
+ln -s $HOME/.vim/bundle/molokai/colors/molokai.vim $HOME/.vim/colors/molokai.vim
+
+#source the bash files
+source $HOME/.bash_profile
+
+#TODO other setup (pip, gems, rbenv global, etc.)
